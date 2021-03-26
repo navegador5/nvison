81a63d22cab3126d1afd511894482935aa163b4b
dli <terryinzaghi@163.com>
2021-03-26 13:38:53 +0000

    test cases

--- nvison/TEST/api-test/quote/quote.tst.js
+++ nvison/TEST/api-test/quote/quote.tst.js
@@ -1,4 +1,23 @@
 const ison = require("/opt/JS/NV5_/nvison/nvison/index");
 ison.CFG.quotes.add("%");
 
-console.log(ison.parse_from_file("quote.ison"));
+var real = ison.parse_from_file("quote.ison");
+
+var should_be = [ 'a\t\x0B\tb', 'cde', 'fgh', 'ijk', 'lmnopq', 'rst \n uvw \n xyz' ]
+
+function eq() {
+    let rslt = true;
+    for(let i=0;i<real.length;i++) {
+        if(real[i] !== should_be[i]){
+            return(false)
+        }
+    }
+    return(rslt)
+}
+
+if(eq()) {
+    console.log(real)
+} else {
+    console.log(real,should_be);
+    throw(new Error("error"))
+}
--- nvison/TEST/api-test/string/str.tst.js
+++ nvison/TEST/api-test/string/str.tst.js
@@ -1,3 +1,12 @@
 const ison = require("/opt/JS/NV5_/nvison/nvison/index")
 
+
+var should_be = [
+  'abc-def',
+  'def\tsquoted',
+  'd    quoted',
+  'tock-quoted-line\ntock-quoted-line',
+  'str@auto-concat@str'
+]
+
 console.log(ison.parse_from_file("./str.ison"))
